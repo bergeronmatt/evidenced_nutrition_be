b31a6029155e1ca72c11ec9bf7f3074633f3e720
ranraok <46853842+ranraok@users.noreply.github.com>
2021-10-26 10:19:54 -0400

    questionnaire form adds cookies

--- src/api/server.js
+++ src/api/server.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 var session = require("express-session");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const secrets = require("../../secrets");
 
 
 // set server to use objects
@@ -19,16 +21,21 @@ server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use(session({
-  name: 'notsession',
-  secret: 'keyboard cat',
-  cookie: {
-    maxAge: 1 * 24 * 60 * 60 * 1000,
-    secure: process.env.SECURE, //set true in production
-    sameSite: process.env.SAME_SITE //set to 'none' in production
+  name: 'SID',
+  secret: process.env.COOKIE_SECRET,
+  genid: function(req) {
+    return Math.random().toString(36).substr(2, 9);;
   },
   httpOnly: true,
   resave: false, 
   saveUninitialized: process.env.SAVE_UNINIT, //set to false for production GDPR laws against setting cookies automatically
+  cookie : {
+    domain: "",
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: "",
+    secure: process.env.SECURE, //set to true for production
+  }
 }));
 
 // server.use(cors({
@@ -60,18 +67,40 @@ server.use(session({
 
 // set cookie when accessing landing page
 
+  generateCookie = (name, value, options) => {
+
+    
+
+  }
+
 server.get("/cookie", (req, res) => {
   
   const id = generateId();
 
   const token = generateToken(id);
-  
+
   res.status(200).json({
     message: 'authorization received',
-    token: token
+    token: token,
   });
 
   console.log('got it');
 });
+
+server.post('/cookie', (req, res) => {
+  // res.json(req.body);
+
+  console.log('session: ', req.session);
+
+  let {path} = req.body;
+  console.log('req.body: ', req.body);
+
+  req.session.cookie.path = path;
+
+  console.log('path: ', req.session.cookie.path);
+
+  res.json(req.session);
+
+})
 
 module.exports = server;
