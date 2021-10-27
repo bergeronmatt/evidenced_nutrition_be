bd9d7a17cd86d6fb364efacd836af7f3670b4160
ranraok <46853842+ranraok@users.noreply.github.com>
2021-10-26 23:18:12 -0400

    checking auth

--- src/api/server.js
+++ src/api/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 
 server.use(cors());
+server.use(helmet());
 server.use(express.json());
 
-const authRouter = require('./auth')
+const auth = require('./auth')
 
 server.get('/api', (req, res) => {
     res.status(200).json({message: 'The api is up and running.'});
 });
 
 // End Points
-server.use('/api/auth', authRouter);
+server.use('/api/auth', auth.router);
 
 module.exports = server;
